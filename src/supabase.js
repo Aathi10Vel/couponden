6835a8223eb285a4fec886217cc273e2753bd90a
unknown <athi1009@gmail.com>
2026-04-18 01:42:30 +0530

    env changes

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://sgfmrhpwocpkkjhnygwn.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNnZm1yaHB3b2Nwa2tqaG55Z3duIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0NTQ3MDAsImV4cCI6MjA5MjAzMDcwMH0.0rBfjdnZGzjQeWAaZy2TumLgkd-1o4Hihvlbw-M6nzI';
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
